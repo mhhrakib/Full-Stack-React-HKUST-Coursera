a347bfd24c0771a222fb2e932f2f36b64f58966a
mhhrakib <mhrakibdu@gmail.com>
2021-05-24 23:57:02 +0600

    Intro. to Redux

--- src/App.js
+++ src/App.js
@@ -1,49 +1,22 @@
-// import logo from './logo.svg';
 import './App.css';
 import { BrowserRouter } from 'react-router-dom';
-// import { Navbar, NavbarBrand } from 'reactstrap';
-// import Menu from './components/MenuComponent';
-// import { DISHES } from './shared/dishes';
-// import React, { Component } from "react";
-// import DishDetail from './components/DishdetailComponent';
-
-
-// class App extends Component {
-//   constructor(props) {
-//     super(props);
-//     this.state = {
-//       dishes: DISHES
-//     };
-//   }
-//   render() {
-//     return (
-//       <div className="App">
-//         <Navbar dark color="primary">
-//           <div className="container">
-//             <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
-//           </div>
-//         </Navbar>
-//         <Menu dishes={this.state.dishes} />
-//       </div>
-//     );
-//   }
-// }
-
-// export default App;
-
-
 import Main from './components/MainComponent';
 import React, { Component } from "react";
+import { Provider } from 'react-redux';
+import { ConfigureStore } from './redux/configureStore';
+const store = ConfigureStore();
 
 class App extends Component {
 
   render() {
     return (
-      <BrowserRouter>
-        <div className="App">
-          <Main />
-        </div>
-      </BrowserRouter>
+      <Provider store={store}>
+        <BrowserRouter>
+          <div className="App">
+            <Main />
+          </div>
+        </BrowserRouter>
+      </Provider>
     );
   }
 }

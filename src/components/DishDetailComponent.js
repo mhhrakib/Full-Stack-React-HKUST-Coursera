28ad3f26108806c1e591dd906e9b7a56ce27dd7d
mhhrakib <mhrakibdu@gmail.com>
2021-05-28 23:44:20 +0600

    Redux Actions

--- src/components/DishDetailComponent.js
+++ src/components/DishDetailComponent.js
@@ -32,7 +32,9 @@ class CommentForm extends Component {
     handleSubmit(values) {
         this.toggleModal();
         console.log('Current State is: ' + JSON.stringify(values));
-        alert('Current State is: ' + JSON.stringify(values));
+        // alert('Current State is: ' + JSON.stringify(values));
+        this.props.addComment(this.props.dishId, values.rating, values.author, values.comment);
+
     }
 
     render() {
@@ -140,7 +142,7 @@ function RenderDish({ dish }) {
         );
 }
 
-function RenderComments({ comments }) {
+function RenderComments({comments, addComment, dishId }) {
     if (comments != null) {
         const comm = comments.map((comment) => {
             return (
@@ -161,7 +163,7 @@ function RenderComments({ comments }) {
         return (
             <div>
                 <ul className='list-unstyled'>{comm}</ul>
-                <CommentForm />
+                <CommentForm dishId={dishId} addComment={addComment} />
             </div>
         )
     }
@@ -199,7 +201,8 @@ const DishDetail = (props) => {
                 </div>
                 <div className="col-12 col-md-5 m-1">
                     <h4> Comments </h4>
-                    <RenderComments comments={props.comments} />
+                    <RenderComments comments={props.comments} addComment={props.addComment}
+        dishId={props.dish.id} />
                 </div>
                 {/* <RenderComments comments={props.dish.comments} /> */}
             </div>
